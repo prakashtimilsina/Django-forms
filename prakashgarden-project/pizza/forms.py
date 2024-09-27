b8b57f2cc85cbae24ca641730d54f1f2ef5d73a8
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-09-27 17:20:25 -0500

    forms model modification

--- prakashgarden-project/pizza/forms.py
+++ prakashgarden-project/pizza/forms.py
@@ -1,10 +1,16 @@
 from django import forms
-from .models import Pizza
+from .models import Pizza, Size
 
 class PizzaForm(forms.ModelForm):
+    size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
+
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
+        labels = {'topping1': 'Topping 1', 'topping2':'Topping 2'}
+
+        # widgets = {'topping1':forms.Textarea}
+        # widgets={'size': forms.CheckboxSelectMultiple}
 
 # class PizzaForm(forms.Form):
 #     topping1 = forms.CharField(label="Topping 1", max_length=100)
