f384160a7d50e83536a09176e97e8972d3c0b68f
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-09-27 16:47:04 -0500

    model form and other forms

---
+++ prakashgarden-project/pizza/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .forms import PizzaForm
+
+def home(request):
+    return render(request, 'pizza/home.html')
+
+def order(request):
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = 'Thanks for ordering. Your  %s %s %s pizza is on the way !' %(filled_form.cleaned_data['size'],filled_form.cleaned_data['topping1'], filled_form.cleaned_data['topping2'],)
+            newform = PizzaForm()
+            return render(request, 'pizza/order.html', {'pizzaform': newform, 'note': note})
+    else:
+        form = PizzaForm()
+        return render(request, 'pizza/order.html', {'pizzaform': form})
